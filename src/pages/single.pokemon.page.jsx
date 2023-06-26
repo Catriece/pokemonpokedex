65926b5749287798465aee4dd858941cae41771e
Catriece Gilbert <119451056+Catriece@users.noreply.github.com>
2023-06-26 14:57:18 -0400

    removed unnecessary code

--- src/pages/single.pokemon.page.jsx
+++ src/pages/single.pokemon.page.jsx
@@ -1,26 +1,5 @@
-//NEEDS WORK AS WELL AS CSS UPDATING
-
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-
-import Bug from "../images/bugtype.png";
-import Dark from "../images/darktype.png";
-import Dragon from "../images/dragontype.png";
-import Electric from "../images/electrictype.png";
-import Fairy from "../images/fairytype.png";
-import Fighting from "../images/fightingtype.png";
-import Fire from "../images/firetype.png";
-import Flying from "../images/flyingtype.png";
-import Ghost from "../images/ghosttype.png";
-import Grass from "../images/grasstype.png";
-import Ground from "../images/groundtype.png";
-import Ice from "../images/icetype.png";
-import Normal from "../images/normaltype.png";
-import Poison from "../images/poisontype.png";
-import Psychic from "../images/psychictype.png";
-import Rock from "../images/rocktype.png";
-import Steel from "../images/steeltype.png";
-import Water from "../images/watertype.png";
 
 function SinglePoketypePage({ pokemon }) {
   let [indivPokemon, setIndivPokemon] = useState(null);
@@ -33,30 +12,9 @@ function SinglePoketypePage({ pokemon }) {
     navigate("/");
   };
 
-  let pokemonTypeArr = pokemon.map((info) => info.type);
-  let pokemonWeaknessArr = pokemon.map((info) => info.weaknesses);
-
   useEffect(() => {
     setIndivPokemon(pokeId);
   }, [pokeId]);
-
-  //WORK ON FILTERING THROUGH POKEMON EVOLUTIONS!
-
-  //   const filterEvolution = (previous, next) => {
-  //     const filteredPokemon = pokemon.filter((pokemon) => {
-  //       const byType =
-  //         type.length === 0 || type.every((type) => pokemon.type.includes(type));
-  //       const byWeakness =
-  //         weakness.length === 0 ||
-  //         weakness.every((weakness) => pokemon.weaknesses.includes(weakness));
-
-  //       return byType && byWeakness;
-  //     });
-
-  //     return filteredPokemon;
-  //   };
-
-  //   const filteredResults = filterPokemon(typeArr, weaknessArr);
 
   return (
     <div id="main-single-pokemon-div">
